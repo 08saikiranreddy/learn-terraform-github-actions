633a769ef6f2ed8a8ad66b1517ace1c32fe82184
08saikiranreddy <saikrn08@gmail.com>
2023-05-23 14:01:57 -0500

    updated organization and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Labs-1207"
+    organization = "ACG-Terraform-Actions-Demo"
 
     workspaces {
       name = "demo-github-actions"
